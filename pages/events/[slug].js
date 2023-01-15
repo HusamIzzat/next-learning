b5ca397b573b830c5ac7d3894835644228cb5afa
HusamIzzat <79759988+HusamIzzat@users.noreply.github.com>
2023-01-16 02:47:50 +0300

    modification from the course

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,14 +1,83 @@
-import { useRouter } from "next/router";
-import Layout from "@/components/layout";
-export default function EventPage() {
-    const router = useRouter();
-    console.log(router)
+
+// import { FaPencilAlt, FaTimes } from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '@/components/Layout'
+import styles from '@/styles/Event.module.css'
+import { useRouter } from 'next/router';
+import { API_URL } from '@/config/index';
+export default function EventPage({ evt }) {
+  const router = useRouter()
+
   return (
     <Layout>
-              <h1>My Event</h1>
-        <h3>{router.query.slug}</h3>
-        <button onClick={()=> router.push('/')}>Back Home</button>
-    </Layout>
+      <div className={styles.event}>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image
+              src={evt.image}
+              width={960}
+              height={600}
+            />
+          </div>
+        )}
 
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+
+        <Link href='/events'>
+          <span className={styles.back}>{'<'} Go Back</span>
+        </Link>
+      </div>
+    </Layout>
   )
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map((evt) => (
+    {
+      params: { slug: evt.slug }
+    }
+  ))
+
+  return {
+    paths,
+    fallback:true
+  } 
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  debugger;
+  const res = await fetch(`${API_URL}/api/events?slug=${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  }
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   }
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
-import Link from "next/link";
 import Layout from '../components/layout'
-export default function Home() {
+import {API_URL} from '@/config/index'
+import EventItem from "@/components/EventItem";
+export default function Home({events}) {
   return (
     <Layout title="Home Page" description="home page events dj">
-      <h1>Home Page</h1>
-      <Link href='/about'>About Page</Link>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events To Show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt}></EventItem>
+      ))}
     </Layout>
   )
 }
+
+
+export async function getStaticProps(){
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return{
+    props:{events},
+    revalidate: 1
+  }
+  
+}
+
